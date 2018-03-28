d4e6ebacc8460d8329a14fd3f7c511f1283ff80e
berdowski <m.s.berdowski@gmail.com>
2018-03-28 12:26:52 +0200

    stage 3

--- js/final-project.js
+++ js/final-project.js
@@ -1,26 +1,223 @@
-(function() {
-  const baseURL = "https://api.github.com"
-  fetch('${baseURL}/users/berdowski')
-    .then(response => response.json())
-    .then(user => {
-      console.log(user.login)
-      console.log(avatar)
-    })
+//on click repositories
+//remove old info and load new info in div cv container
+//when click on cv, go back to page cv (load cv info in div cv container)
+//
 
-    const repoDiv =
-    document.getElementById("repositories")
 
-    fetch("https://api.github.com/users/berdowski/repos")
-      .then(response => response.json())
-      .then (repositories => {
+// function openNav(evt,navName) {
+//   // Declare all variables
+//   let i, repo, navContent;
 
-      for (let repo of repositories) {
-        const repoParagraph = document.createElement("p")
-        repoParagraph.innerText = repo.name
-        repoDiv.appendChild(repoParagraph)
-      }
-    })
-  })
+//   // Get all elements with class="repo" and hide them
+//   repo = document.getElementById("repo");
+//   for (i = 0; i < repo.length; i++) {
+//       repo[i].style.display = "none";
+//   }
+
+//   // Get all elements with class="navContent" and remove the class "active"
+//   navContent = document.getElementsByClassName("navContent");
+//   for (i = 0; i < navContent.length; i++) {
+//       navContent[i].className = navContent[i].className.replace("active", "");
+//   }
+
+//   // Show the current tab, and add an "active" class to the button that opened the tab
+//   document.getElementById(navName).style.display = "block";
+//   evt.currentTarget.className += "active";
+// }
+
+
+(function () {
+
+const portofolio = document.getElementById("pf")
+portofolio.addEventListener("click", function(){
+  
+  function openNav(evt,navName) {
+    // Declare all variables
+    let i, repo, navContent;
+  
+    // Get all elements with class="repo" and hide them
+    repo = document.getElementById("repo");
+    for (i = 0; i < repo.length; i++) {
+        repo[i].style.display = "none";
+    }
+  
+    // Get all elements with class="navContent" and remove the class "active"
+    navContent = document.getElementsByClassName("navContent");
+    for (i = 0; i < navContent.length; i++) {
+        navContent[i].className = navContent[i].className.replace("active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(navName).style.display = "block";
+    evt.currentTarget.className += "active";
+  }  
+
+})
+
+  // const baseURL = "https://api.github.com"
+  // fetch('${baseURL}/users/berdowski')
+  //   .then(response => response.json())
+  //   .then(user => {
+  //     console.log(user.login)
+
+      const repoDiv=document.getElementById("repo")
+
+      fetch("https://api.github.com/users/berdowski/repos")
+          .then(response => response.json())
+          .then (repositories => {
+
+      for (let repo of repositories){
+          const repositoryName = document.createElement("h1")
+          repositoryName.innerText = repo.name
+          repository.appendChild(repositoryName)
+
+          const repositoryDescription = document.createElement("p")
+          repositoryDescription.innerText = repo.description
+          repository.appendChild(repositoryDescription)
+
+          const repositoryLanguage = document.createElement("p")
+          repositoryLanguage.innerText = repo.language
+          repository.appendChild(repositoryLanguage)
+
+          const repository = document.createElement("div")
+          repoDiv.appendChild(repository)
+           } 
+          })
+  }
+)
+ 
+
+ 
+    
+
+    
+
+// get the div where the repositories will be added, with id "repo"
+       // <div id="repo"></div>
+
+       // for every repository in repositories
+       
+       //     create an outer div (<div class="repository">)
+
+       //     create a h1 with repository name (<h1><repository name></h1>)
+       //     add h1 to outer div
+
+       //     create a p with repo description (<p>Description: <repository)description></p>
+       //     add p to outer div
+
+       //     create a p with repo language (<p>Language: <repository language></p>)
+       //     add p to outer div
+
+       //     add outer div to repo div
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //   const repoDiv =
+  //   document.getElementById("repositories")
+
+  //   fetch("https://api.github.com/users/berdowski/repos")
+  //     .then(response => response.json())
+  //     .then (repositories => {
+
+  //     for (let repo of repositories) {
+  //       const repoParagraph = document.createElement("p")
+  //       repoParagraph.innerText = repo.name
+  //       repoDiv.appendChild(repoParagraph)
+  //     }
+  //   })
+  // }
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("pf").addEventListener("click", replaceTitle)
+// document.getElementById("cv").addEventListener("click", replaceTitle2)
+
+// function replaceTitle (title) {
+//   const fieldRenameTitle = document.getElementById("title")
+//   fieldRenameTitle.innerHTML="Repositories" 
+// }
+// function replaceTitle2 (title) {
+//   const fieldRenameTitle = document.getElementById("title")
+//   fieldRenameTitle.innerHTML="Maaike Berdowski" 
+// }
+// function replaceName (name) {
+//   const fieldRenameName = document.getElementById("name")
+//   fieldRenameName.innerText="user.login"
+// } 
+
+// function replaceParagraphTitles (ptitles) {
+//   const fieldRenamePT = document.getElementById("ptitles")
+//   fieldRenamePT.innerText="repotitles"
+// }
+
+// function replaceParagraph (p) {
+// }
+
+
+
+// function loadPF() {
+
+//   const baseURL = "https://api.github.com"
+//   fetch('${baseURL}/users/berdowski')
+//     .then(response => response.json())
+//     .then(user => {
+//       console.log(user.login)
+//     })
+
+//     const repoDiv =
+//     document.getElementById("repositories")
+
+//     fetch("https://api.github.com/users/berdowski/repos")
+//       .then(response => response.json())
+//       .then (repositories => {
+
+//       for (let repo of repositories) {
+//         const repoParagraph = document.createElement("p")
+//         repoParagraph.innerText = repo.name
+//         repoDiv.appendChild(repoParagraph)
+//       }
+//     })
+//   }
 
 
   // TODO: load CV tab
@@ -79,3 +276,4 @@
 //     })
 //   }
 // })()
+    
